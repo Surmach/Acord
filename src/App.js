b3820c459582f66d7da5156171b9df6e0ae0a1a1
unknown <curmachevskiy@gmail.com>
2019-11-15 17:51:29 +0200

    123

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div>
       <MainMap></MainMap>
+      
     </div>
     
   );
--- src/components/mapBox/mainMap.js
+++ src/components/mapBox/mainMap.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import styled from "styled-components";
+import MapboxGeocoder from "mapbox-gl-geocoder"
+
+
 mapboxgl.accessToken =
   "pk.eyJ1Ijoic3VybWFjaCIsImEiOiJjazJmcnQ0YngwM3k1M2hwZjVvY2ZkMnBvIn0.mvj8uM-XUma34v5Y9SiwNA";
 
@@ -16,11 +19,22 @@ export default function MainMap() {
       center: [lng, lat],
       zoom: zoom
     });
+
+    var geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+      });
+       
+      document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+
   });
 
   return (
     <div>
       <MapBoxComponent id="mapBoxContainer"></MapBoxComponent>
+      <Geocoder id='geocoder' class='geocoder'></Geocoder>
+      
     </div>
   );
 }
@@ -32,3 +46,12 @@ const MapBoxComponent = styled.div`
   left: 0;
   bottom: 0;
 `;
+
+const Geocoder = styled.div`
+position:absolute;
+z-index:1;
+width:50%;
+left:50%;
+margin-left:-25%;
+top:20px;
+`
